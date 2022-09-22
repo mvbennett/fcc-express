74aba578b579e4f54b773a823f1ab850e9fe07cb
Mike Bennett <mvbennett9@gmail.com>
2022-09-22 23:14:07 +0900

    all challenges until root middleware complete

--- myApp.js
+++ myApp.js
@@ -1,16 +1,36 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
+app.use(function middleware(req, res, next) {
+  let string = `${req.method} ${req.path} - ${req.ip}`;
+  console.log(string);
+  next();
+});
 
+// console.log('Hello World');
 
+// app.get('/', (req, res) => {
+//   res.send('Hello Express');
+// });
 
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
 
+app.use('/public', express.static(__dirname + '/public'));
 
-
-
-
-
-
+app.get('/json', (req,res) => {
+  let obj = {'message': 'Hello json'};
+  switch (process.env['MESSAGE_STYLE']) {
+    case 'uppercase':
+        obj.message = obj.message.toUpperCase();
+      break;
+    default:
+      break;
+  }
+  res.json(obj);
+});
 
 
 
